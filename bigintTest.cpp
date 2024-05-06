96c311b63b78236db6143b6e31b8ec3a1019d5ba
arnavmishra1 <arnav9@outlook.com>
2024-05-06 15:28:45 -0700

    operator+ working

--- bigintTest.cpp
+++ bigintTest.cpp
@@ -10,6 +10,7 @@
 #include <ostream>
 #include <vector>
 #include <math.h>
+#include <iomanip>
 
 
 using namespace std;
@@ -19,8 +20,9 @@ class BigInt {
         vector<char> digits;
         BigInt fiboHelper(BigInt n, BigInt a = 0, BigInt b = 1);
     public:
-        BigInt();
+        BigInt() {}
         BigInt(int a) {
+            digits = {};
             if (a == 0) {
                 digits.push_back('0');
             }
@@ -32,6 +34,7 @@ class BigInt {
             }
         }
         BigInt(string a) {
+            digits = {};
             if (a.length() == 0) {
                 digits.push_back('0');
             } else {
@@ -44,9 +47,9 @@ class BigInt {
         }
 
         BigInt operator+ (BigInt a) {
-            vector<int> temp = {};
+            BigInt ret;
+            vector<int> temp;
             vector<int> carry;
-            int loop;
             if (digits.size() > a.size()) {
                 while (digits.size() > a.size()) {
                     a.digits.push_back('0');
@@ -57,16 +60,36 @@ class BigInt {
                 }
             }
 
+            cout << *this << endl;
+            cout << a << endl;
+
+
             for (int i = 0; i < digits.size(); i++) {
-                temp.push_back(char(digits[i] + a.digits[i]));
+                cout << "c: " << int(digits[i]) - 48 + int(a.digits[i]) - 48 << endl;
+                temp.push_back((int(digits[i]) - 48 + int(a.digits[i]) - 48));
+                cout << temp[i] << endl;
             }
 
+            temp.push_back(0);
             for (int i = 0; i < temp.size(); i++) {
-                if (int(temp[i]) > 9) {
-                    temp[i] = char(int(temp[i]) - 10);
-                    temp[i+1] = char(int(temp[i+1]) + 1);
+                if (temp[i] > 9) {
+                    cout << "!" << endl;
+                    temp[i] = temp[i] - 10;
+                    temp[i+1] = temp[i+1] + 1;
                 }
             }
+            
+            reverse(temp.begin(), temp.end());
+            int i = 0;
+            if (temp[0] == 0) {
+                i = 1;
+            }
+            string fin;
+            for (i; i < temp.size(); i++) {
+                fin += to_string(temp[i]);
+            }
+            ret = fin;
+            return ret;
         }
         BigInt operator- (BigInt);
         BigInt operator- (int);
@@ -80,8 +103,9 @@ class BigInt {
         }
 
         void print() {
-            for (int i : digits) {
-                cout << i;
+            vector<char>::iterator itr;
+            for (itr = digits.end()-1; itr >= digits.begin(); itr--) {
+                cout << *itr;
             }
             cout << endl;
         }
@@ -113,10 +137,51 @@ class BigInt {
         friend BigInt operator+ (int, BigInt);
 };
 
+/*void testUnit() {
+    int space = 10;
+    cout << "\a\nTestUnit:\n"<<flush;
+    system("whoami");
+    system("date");
+    // initialize variables
+    BigInt n1(25);
+    BigInt s1("25");
+    BigInt n2(1234);
+    BigInt s2("1234");
+    BigInt n3(n2);
+    BigInt fibo(12345);
+    BigInt fact(50);
+    BigInt imax = INT_MAX;
+    BigInt big("9223372036854775807");
+    // display variables
+    cout << "n1(int) :"<<setw(space)<<n1<<endl;
+    cout << "s1(str) :"<<setw(space)<<s1<<endl;
+    cout << "n2(int) :"<<setw(space)<<n2<<endl;
+    cout << "s2(str) :"<<setw(space)<<s2<<endl;
+    cout << "n3(n2) :"<<setw(space)<<n3<<endl;
+    cout << "fibo(12345):"<<setw(space)<<fibo<<endl;
+    cout << "fact(50) :"<<setw(space)<<fact<<endl;
+    cout << "imax :"<<setw(space)<<imax<<endl;
+    cout << "big :"<<setw(space)<<big<<endl;
+    cout << "big.print(): "; big.print(); cout << endl;
+    cout << n2 << "/"<< n1<< " = "<< n2/n1 <<" rem "<<n2%n1<<endl;
+    cout << "fibo("<<fibo<<") = "<<fibo.fibo() << endl;
+    cout << "fact("<<fact<<") = "<<fact.fact() << endl;
+    cout << "10 + n1 = " << 10+n1 << endl;
+    cout << "n1 + 10 = " << n1+10 << endl;
+    cout << "(n1 == s1)? --> "<<((n1==s1)?"true":"false")<<endl;
+    cout << "n1++ = ? --> before:"<<n1++<<" after:"<<n1<<endl;
+    cout << "++s1 = ? --> before:"<<++s1<<" after:"<<s1<<endl;
+    cout << "s2 * big = ? --> "<< s2 * big<<endl;
+    cout << "big * s2 = ? --> "<< big * s2<<endl;
+}*/
+
 int main() {
     //main function
     //testUnit();
-    BigInt myNum("12345678901234567890");
+    BigInt myNum("01");
+    BigInt yourNum(900);
     cout << myNum << endl;
+    cout << yourNum << endl;
+    cout << (myNum + yourNum) << endl;
     return 0;
 }
